9f071ead7ae6a419f4df9114620316638e9aa11a
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-04-11 14:58:36 +0200

    update

--- Facade/src/main/java/com/example/facade/controller/TodoFacadeController.java
+++ Facade/src/main/java/com/example/facade/controller/TodoFacadeController.java
@@ -1,10 +1,7 @@
 package com.example.facade.controller;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/facade/todos")
@@ -35,30 +24,12 @@ public class TodoFacadeController {
         this.restTemplate = restTemplate;
     }
 
-    @SneakyThrows
     @Retry(name = INSTANCE)
     @CircuitBreaker(name = INSTANCE)
-    @RateLimiter(name = INSTANCE)
     @GetMapping("/{id}")
     public ResponseEntity<?> getTodoById(@PathVariable("id") int id) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        var circuitBreakerRegistry = CircuitBreakerRegistry.ofDefaults();
-        var state = circuitBreakerRegistry.circuitBreaker(INSTANCE).getState();
+        System.out.println("--> FacadeService SENT Request");
         var response = restTemplate.getForObject("%s/%s".formatted(URL, id), String.class);
         return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    public ResponseEntity<?> cbFallback(Exception e) {
-        var message = e.getMessage().contains(INSTANCE)
-                                    ? e.getMessage()
-                                    : "Please try again later";
-        return new ResponseEntity<>(message, HttpStatus.SERVICE_UNAVAILABLE);
-    }
-
-    public ResponseEntity<?> retryFallback(Exception e) {
-        var message = e.getMessage().contains(INSTANCE)
-                ? e.getMessage()
-                : "Could not connect to todo service after 5 attempts";
-        return new ResponseEntity<>(message, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
